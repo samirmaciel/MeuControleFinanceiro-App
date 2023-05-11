1b05fc29b8a07af1948c68ec6f22d358b1c62a87
Samir Maciel <samirmaciel@mc1global.com>
2023-05-11 19:00:49 -0300

    create register new user screen

--- app/src/main/java/com/samirmaciel/meucontrolefinanceiro/view/registerNewUser/RegisterNewUserFragment.kt
+++ app/src/main/java/com/samirmaciel/meucontrolefinanceiro/view/registerNewUser/RegisterNewUserFragment.kt
@@ -1,30 +1,31 @@
-package com.samirmaciel.meucontrolefinanceiro.view.login
+package com.samirmaciel.meucontrolefinanceiro.view.registerNewUser
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.samirmaciel.meucontrolefinanceiro.R
-import com.samirmaciel.meucontrolefinanceiro.databinding.FragmentLoginBinding
+import com.samirmaciel.meucontrolefinanceiro.databinding.FragmentRegisterNewUserBinding
 
 
-class LoginFragment : Fragment(R.layout.fragment_login) {
+class RegisterNewUserFragment : Fragment(R.layout.fragment_register_new_user) {
 
-    private var binding : FragmentLoginBinding? = null
+   private var binding : FragmentRegisterNewUserBinding? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         bindView(view)
     }
 
     private fun bindView(view: View) {
-        binding = FragmentLoginBinding.bind(view)
+        binding = FragmentRegisterNewUserBinding.bind(view)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         binding = null
     }
-
 }
