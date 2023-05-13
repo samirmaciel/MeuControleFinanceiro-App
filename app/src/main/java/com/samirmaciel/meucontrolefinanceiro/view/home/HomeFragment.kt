2f5edd94d4d3613e23b9eb9c8a6c97766dd4b932
Samir Maciel <samirmaciel@mc1global.com>
2023-05-13 10:55:50 -0300

    create home screen

---
+++ app/src/main/java/com/samirmaciel/meucontrolefinanceiro/view/home/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.samirmaciel.meucontrolefinanceiro.view.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.samirmaciel.meucontrolefinanceiro.R
+import com.samirmaciel.meucontrolefinanceiro.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var binding : FragmentHomeBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bindView(view)
+    }
+
+    private fun bindView(view: View) {
+        binding = FragmentHomeBinding.bind(view)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
