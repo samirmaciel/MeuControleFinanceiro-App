9e43066a2f47b9da6e496d9cc11d8f4e13f957a0
Samir Maciel <samirmaciel@mc1global.com>
2023-05-15 11:41:36 -0300

    create my credit card screen

---
+++ app/src/main/java/com/samirmaciel/meucontrolefinanceiro/view/myCreditCards/MyCreditCardsFragment.kt
@@ -0,0 +1,29 @@
+package com.samirmaciel.meucontrolefinanceiro.view.myCreditCards
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.samirmaciel.meucontrolefinanceiro.R
+import com.samirmaciel.meucontrolefinanceiro.databinding.FragmentMyCreditCardsBinding
+
+class MyCreditCardsFragment : Fragment(R.layout.fragment_my_credit_cards) {
+
+    private var binding : FragmentMyCreditCardsBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bindView(view)
+    }
+
+    private fun bindView(view: View) {
+        binding = FragmentMyCreditCardsBinding.bind(view)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
